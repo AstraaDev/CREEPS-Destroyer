e840d2497f29782c08af04cce7cd4daddbb070b7
a5traa <43183806+AstraaDev@users.noreply.github.com>
2025-02-10 20:47:47 +0100

    refacto + small opti

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,53 +1,31 @@
+import action.Action;
 import action.ServerCall;
 import com.epita.creeps.given.json.Json;
-import com.epita.creeps.given.vo.Resources;
-import com.epita.creeps.given.vo.geometry.Point;
 import com.epita.creeps.given.vo.response.InitResponse;
-import mobs.*;
 import com.mashape.unirest.http.exceptions.UnirestException;
-
 import java.util.*;
 
 public class Program {
 
-    public static String login;
-    public static Resources resources = new Resources();
-    public static Resources resourcesFutur = new Resources();
-    public static Point hdv;
-    public static int gcTickRate;
-    public static int serverTick;
-
-
     public static void main(String[] args) throws UnirestException, InterruptedException {
-        boolean dev = false;
-        ServerCall call;
-        if (dev)
-        {
-            login= usingRandomUUID();
-            call = new ServerCall("http://" + args[0] + ":" + args[1], login);
-            System.out.println("New login: " + login);
-        }
-        else
-        {
-            call = new ServerCall("http://" + args[0] + ":" + args[1], args[2]);
-        }
+        boolean devMode = true;
 
+        String url = args[0];
+        int port = Integer.parseInt(args[1]);
+        String login = (devMode) ? args[2] + UUID.randomUUID(): args[2];
+
+        // Connecting to the server
+        System.out.println("Connecting to " + url + ":" + port + " as login: " + login);
+        System.out.println("-------------------------------------------------------");
+
+        // ServerCall instance declaration
+        ServerCall call = new ServerCall(url, port, login);
+
+        // Parsing
         InitResponse initResponse = Json.parse(call.postInit(), InitResponse.class);
+
+        // Commands instance declaration
         double timeout = 1000/initResponse.setup.ticksPerSeconds;
-        hdv = initResponse.townHallCoordinates;
-
-        gcTickRate = initResponse.setup.gcTickRate;
-        serverTick = initResponse.tick;
-
-        //ResourceMan resourceMan = new ResourceMan(initResponse,call,timeout,initResponse.citizen1Id);
-        //resourceMan2.start();
-    }
-
-    static String usingRandomUUID() {
-
-        UUID randomUUID = UUID.randomUUID();
-
-        return randomUUID.toString().replaceAll("_", "");
-
+        Action action = new Action(initResponse, call, timeout);
     }
 }
--- src/main/java/action/ServerCall.java
+++ src/main/java/action/ServerCall.java
@@ -1,44 +1,39 @@
 package action;
 
-import com.epita.creeps.given.exception.NoReportException;
-import com.epita.creeps.given.json.Json;
-import com.epita.creeps.given.vo.report.Report;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class ServerCall {
 
-    private String url;
-    private String login;
+    public String url;
+    public int port;
+    public String login;
 
-    public ServerCall(String url, String login) {
+    public ServerCall(String url, int port, String login) {
         this.url = url;
+        this.port = port;
         this.login = login;
     }
 
     public String getStatus() throws UnirestException {
-        return Unirest.get(this.url+"/status").asJson().getBody().toString();
+        return Unirest.get("http://" + this.url + ':' + this.port + "/status").asJson().getBody().toString();
     }
 
     public String getStatistics() throws UnirestException {
-        return Unirest.get(this.url+"/statistics").asJson().getBody().toString();
-    }
-
-    public Report getReport(String id) {
-        try {
-            return Json.parseReport(Unirest.get(this.url+"/report/"+id).asJson().getBody().toString());
-        }
-        catch (UnirestException | NoReportException e) {
-            return null;
-        }
-
+        return Unirest.get("http://" + this.url + ':' + this.port + "/statistics").asJson().getBody().toString();
     }
 
     public String postInit() throws UnirestException {
-        return Unirest.post(this.url+"/init/"+this.login).body("{}").asJson().getBody().toString();
+        return Unirest.post("http://" + this.url + ':' + this.port + "/init/" + this.login).body("{}").asJson().getBody().toString();
     }
 
-    public String postCommand(String unitId, String opCode, String body) throws UnirestException {
-        return Unirest.post(this.url+"/command/"+this.login+"/"+unitId+"/"+opCode).body(body).asJson().getBody().toString();
+    public String postCommand(String unitId, String opcode, String body) throws UnirestException {
+        if (body == null)
+            body = "{}";
+        return Unirest.post("http://" + this.url + ':' + this.port + "/command/" + this.login + "/" + unitId + "/" + opcode).body(body).asJson().getBody().toString();
+    }
+
+    public String getReport(String reportId) throws UnirestException {
+        return Unirest.get("http://" + this.url + ':' + this.port + "/report/" + reportId ).asJson().getBody().toString();
     }
 }
